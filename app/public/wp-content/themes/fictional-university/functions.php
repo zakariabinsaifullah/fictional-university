2639e3577c9b7c5d1b64b84444665316706885eb
Zakaria Binsaifullah <zbinsaifullah@gmail.com>
2022-02-07 12:06:29 +0600

    enqueue scripts

--- app/public/wp-content/themes/fictional-university/functions.php
+++ app/public/wp-content/themes/fictional-university/functions.php
@@ -28,20 +28,22 @@ function page_banner($args = NULL){
 <?php }
 // The end....
 function university_files() {
-	wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCczfPrVqyYOMYLWg9seB9KRWMKC9nRAd0', NULL, '1.0', true);
-  wp_register_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), array('jquery'), '1.0', true);
-  wp_enqueue_script( 'main-university-js' );
 	wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 	wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 	wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
+
+	wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCczfPrVqyYOMYLWg9seB9KRWMKC9nRAd0', NULL, '1.0', true);
+	wp_enqueue_script('main-university-js', get_template_directory_uri() . '/js/scripts-bundled.js', array('jquery'), '1.0', true );
+	// wp_enqueue_script( 'main-university-js' );
 }
+add_action('wp_enqueue_scripts', 'university_files');
+
 
 function new_excerpt_more( $more ) {
 	return '';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
-add_action('wp_enqueue_scripts', 'university_files');
 function university_features() {
 	add_theme_support('title-tag');
 	add_theme_support('post-thumbnails');
